6932d98e99db675a2c3e09c6b3d2ca5fc94c3a64
Hillary Kipkemoi <tenskap@gmail.com>
2024-07-27 14:03:25 +0300

    add func to get context and answer for evaluation_data

--- src/ragas/ragas_pipeline.py
+++ src/ragas/ragas_pipeline.py
@@ -11,6 +11,8 @@ from ragas.metrics import (
 )
 
 from src.ragas.ragas_utils import load_evaluation_data
+from typing import List, Dict
+from langchain.docstore.document import Document
 
 def evaluate_metrics(dataset):
   # evaluating dataest on listed metrics
@@ -28,3 +30,29 @@ def evaluate_metrics(dataset):
   df_results = result.to_pandas()
 
   return df_results
+
+
+def get_context_and_answer(
+    evaluation_data: List[Dict[str, List[str]]],
+    rag_chain, 
+) -> List[Dict[str, str]]:
+
+    results = []
+
+    for question, ground_truth in zip(
+        evaluation_data["questions"], evaluation_data["ground_truths"]
+    ):
+        result = rag_chain.invoke(question)
+        contexts = [doc.page_content for doc in result["source_documents"]]
+        concatenated_context = " ".join(contexts)  
+
+        results.append(
+            {
+                "question": question,
+                "context": concatenated_context,
+                "answer": result["result"],
+                "ground_truth": ground_truth,
+            }
+        )
+
+    return results
