1fc3a6495440a4d90299a8ed020cdf7148d3022f
Hillary Kipkemoi <tenskap@gmail.com>
2024-07-27 14:08:25 +0300

    add contexts as list and docstrings

--- src/ragas/ragas_pipeline.py
+++ src/ragas/ragas_pipeline.py
@@ -11,6 +11,7 @@ from ragas.metrics import (
 )
 
 from src.ragas.ragas_utils import load_evaluation_data
+from src import pretty_print_docs
 from typing import List, Dict
 from langchain.docstore.document import Document
 
@@ -36,23 +37,39 @@ def get_context_and_answer(
     evaluation_data: List[Dict[str, List[str]]],
     rag_chain, 
 ) -> List[Dict[str, str]]:
+    """Retrieves context and generates answers for each question in the evaluation data.
+
+    Args:
+        evaluation_data (Dict[str, List[str]]): A dictionary containing:
+            - "questions": A list of questions.
+            - "ground_truths": A list of corresponding ground truth answers.
+        rag_chain: The RAG chain instance to use for retrieval and generation.
+
+    Returns:
+        List[Dict[str, str]]: A list of dictionaries, each containing:
+            - "question": The original question.
+            - "context": A string of concatenated relevant contexts.
+            - "answer": The generated answer from the RAG chain.
+            - "ground_truth": The ground truth answer (from the evaluation data).
+    """
 
     results = []
 
     for question, ground_truth in zip(
         evaluation_data["questions"], evaluation_data["ground_truths"]
     ):
-        result = rag_chain.invoke(question)
-        contexts = [doc.page_content for doc in result["source_documents"]]
-        concatenated_context = " ".join(contexts)  
+        response = rag_chain.invoke(question)
+        contexts_list = [doc.page_content for doc in response["source_documents"]]
+                
+        test_data = {
+            "question": question,
+            "context": contexts_list,
+            "answer": response["result"],
+            "ground_truth": ground_truth,
+        }
 
         results.append(
-            {
-                "question": question,
-                "context": concatenated_context,
-                "answer": result["result"],
-                "ground_truth": ground_truth,
-            }
+            
         )
 
     return results
