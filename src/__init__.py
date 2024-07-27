f4f0ea215197b157fb27f069efe71beb88fe3900
Hillary Kipkemoi <tenskap@gmail.com>
2024-07-27 18:24:15 +0300

    move display_df func to script with added docs/error handling

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,2 @@
 from .env_loader import load_api_keys
-from .utils import load_and_process_dataset, pretty_print_docs
+from .utils import load_and_process_dataset, pretty_print_docs, display_df
--- src/utils.py
+++ src/utils.py
@@ -27,11 +27,11 @@ def pretty_print_docs(docs):
     """
     Prints the content of documents in a formatted manner.
 
-    Parameters:
-    - docs (list): A list of document objects. Each document object must have a 'page_content' attribute.
+    Args:
+        - docs (list): A list of document objects. Each document object must have a 'page_content' attribute.
 
     Returns:
-    None. This function prints the content of each document to the console.
+        None. This function prints the content of each document to the console.
     """
     try:
         # Check if docs is a list
@@ -51,3 +51,31 @@ def pretty_print_docs(docs):
         )
     except Exception as e:
         print(f"An error occurred while printing documents: {e}")
+
+def display_df(df: pd.DataFrame, n_rows: int = 5, head_or_tail: str = "head") -> None:
+    """
+    Displays a DataFrame in markdown format.
+
+    Args:
+        df (pd.DataFrame): The DataFrame to display.
+        n_rows (int): The number of rows to display. Default is 5.
+        head_or_tail (str): Whether to display the head or tail of the DataFrame. 
+                            Must be either 'head' or 'tail'. Default is 'head'.
+
+    Returns: None.
+
+    Raises:
+        ValueError: If head_or_tail is not 'head' or 'tail'.
+    """
+    
+    if head_or_tail not in {"head", "tail"}:
+        raise ValueError("head_or_tail must be either 'head' or 'tail'")
+    
+    if df.empty:
+        print("DataFrame is empty")
+        return
+    
+    if head_or_tail == "head":
+        print(df.head(n_rows).to_markdown(index=False, numalign="left", stralign="left"))
+    else:  # head_or_tail == "tail"
+        print(df.tail(n_rows).to_markdown(index=False, numalign="left", stralign="left"))
