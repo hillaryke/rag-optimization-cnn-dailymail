7b3fe9fe962024646b702f725e15d0aada8138aa
Hillary Kipkemoi <tenskap@gmail.com>
2024-07-28 07:10:39 +0300

    update recursive split strategy docs, remove unnecessary logs

--- src/rag_pipeline/chunking_strategies.py
+++ src/rag_pipeline/chunking_strategies.py
@@ -1,14 +1,10 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
 from langchain_openai.embeddings import OpenAIEmbeddings
-from langchain_experimental.text_splitter import SemanticChunker
 
 def chunk_by_recursive_split(documents: list[Document], chunk_size: int = 1000, chunk_overlap: int = 0) -> list[Document]:
     """
-    Splits documents into chunks of a specified size using a recursive character-based approach.
-
-    This function takes a list of documents and splits each one into smaller chunks based on a specified character count,
-    using a RecursiveCharacterTextSplitter. This method does not consider semantic content, and splits are based purely on
+    Splits a list documents into chunks of a specified size using a recursive character-based approach. Splits are based purely on
     character count.
 
     Args:
@@ -27,10 +23,6 @@ def chunk_by_recursive_split(documents: list[Document], chunk_size: int = 1000, 
         chunks = text_splitter.split_documents(documents)
         print(f"--Split {len(documents)} documents into {len(chunks)} chunks.--")
 
-        document = chunks[10] if len(chunks) > 10 else None
-        if document:
-            print(document.page_content)
-            print(document.metadata)
     except Exception as e:
         print(f"Error during recursive split: {e}")
         chunks = []  # Ensure chunks is defined even in case of error
