cc72425f6c084e6e08cea67329c64a55558c6f49
Hillary Kipkemoi <tenskap@gmail.com>
2024-07-28 17:25:33 +0300

    Update RAGsystem to use pgvector replacing Chroma

--- src/rag_pipeline/rag_system.py
+++ src/rag_pipeline/rag_system.py
@@ -4,11 +4,11 @@ import pandas as pd
 from typing import List, Dict, Any
 from dotenv import load_dotenv
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_chroma import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
 from langchain.retrievers import EnsembleRetriever
 from langchain_community.retrievers import BM25Retriever
+from langchain_postgres import PGVector
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -17,18 +17,24 @@ from src.rag_pipeline.rag_utils import rag_chain_setup
 from src.rag_pipeline.chunking_strategies import chunk_by_recursive_split
 from src.rag_pipeline.load_docs import load_docs_from_csv
 
-CHROMA_PATH = "chromadb"
+# constants - can be easily moved to a config file
+PG_CONNECTION_STRING = os.getenv("PG_CONNECTION_STRING")
+COLLECTION_NAME = "cnn_dailymail_validation_subset"
+SOURCE_FILE_PATH = "data/cnn_dailymail_validation_subset.csv"
+CHUNK_SIZE = 1000
+CHUNK_OVERLAP = 200
 
 class RAGSystem:
     def __init__(self, 
                 model_name: str,
                 embeddings: Any = None,
-                source_file_path: 
-                str = "data/cnn_dailymail_validation_subset.csv",
-                existing_chroma: str = False,
+                collection_name: str = COLLECTION_NAME,
+                source_file_path: str = SOURCE_FILE_PATH,
+                existing_vectorstore: str = False,
+                clear_store: bool = False,
                 use_ensemble_retriever: bool = False,
-                chunk_size: int = 1000,
-                chunk_overlap: int = 200
+                chunk_size: int = CHUNK_SIZE,
+                chunk_overlap: int = CHUNK_OVERLAP
     ):
         self.model_name = model_name
         self.llm = None
@@ -37,9 +43,10 @@ class RAGSystem:
         self.embeddings = OpenAIEmbeddings()
         self.vectorstore = None
         self.rag_chain = None
-        self.existing_chroma = existing_chroma
+        self.clear_store = clear_store
         self.vectorstore_retriever = None
         self.bm25_retriever = None
+        self.existing_vectorstore = existing_vectorstore
         self.ensemble_retriever = None
         self.use_ensemble_retriever = use_ensemble_retriever
         self.chunk_size = 1000
@@ -53,14 +60,37 @@ class RAGSystem:
     def prepare_documents(self, chunk_size: int = 1000, chunk_overlap: int = 200):
         split_docs = chunk_by_recursive_split(self.documents, chunk_size=chunk_size, chunk_overlap=chunk_overlap)
         return split_docs
+    
+    def initialize_vectorstore(self):
+        self.vectorstore = PGVector(
+            embeddings=self.embeddings,
+            collection_name=COLLECTION_NAME,
+            connection=PG_CONNECTION_STRING,
+            use_jsonb=True
+        )
+        return self.vectorstore
 
     def setup_vectorstore(self, split_docs: List[str] = None):
-        if split_docs is None:
-            # Load an existing Chroma instance
-            self.vectorstore = Chroma(persist_directory=CHROMA_PATH, embedding_function=self.embeddings)
-        else:
-            # Create a new Chroma instance
-            self.vectorstore = Chroma.from_documents(split_docs, embedding=self.embeddings, persist_directory=CHROMA_PATH)
+        # For testing
+        split_docs = split_docs[:3]
+        # Initialize the vectorstore - this could be an existing collection
+        self.initialize_vectorstore()
+        
+        if self.clear_store:
+            self.vectorstore.drop_tables()
+        
+        if split_docs:
+            self.initialize_vectorstore()
+            self.vectorstore.add_documents(split_docs)
+
+            # caclculate the embedding cost if using openai embeddings
+            # check instance of embeddings if OpenAIEmbeddings
+            # if isinstance(self.embeddings, OpenAIEmbeddings):
+                # calculate cost here
+            
+            # Get the existing vectorstore collection
+            
+            # Add documents to the vectorstore
 
     def setup_bm25_retriever(self, split_docs: List[str]):
         self.bm25_retriever = BM25Retriever.from_documents(split_docs)
@@ -95,7 +125,7 @@ class RAGSystem:
     def initialize(self):
         self.load_documents()
         
-        if self.existing_chroma:
+        if self.existing_vectorstore:
             self.setup_vectorstore()
             self.setup_basic_retriever()
         else:
