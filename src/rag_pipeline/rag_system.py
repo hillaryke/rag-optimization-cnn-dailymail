11b8a50cf69e7671ab21b83433e5aa5c6ed9ce94
Hillary Kipkemoi <tenskap@gmail.com>
2024-07-29 18:10:08 +0300

    Add multiquery retriever, Refactor RAGSystem setup and retriever configuration

--- src/rag_pipeline/rag_system.py
+++ src/rag_pipeline/rag_system.py
@@ -9,6 +9,7 @@ from langchain.chains import RetrievalQA
 from langchain.retrievers import EnsembleRetriever
 from langchain_community.retrievers import BM25Retriever
 from langchain_postgres import PGVector
+from langchain.retrievers.multi_query import MultiQueryRetriever
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -35,22 +36,27 @@ class RAGSystem:
                 existing_vectorstore: str = False,
                 clear_store: bool = False,
                 use_ensemble_retriever: bool = False,
+                use_multiquery: bool = False,
                 chunk_size: int = CHUNK_SIZE,
                 chunk_overlap: int = CHUNK_OVERLAP
     ):
         self.model_name = model_name
         self.llm = None
+        self.llm_queries_generator = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
         self.source_file_path = source_file_path
         self.documents = []
+        self.split_docs = []
         self.embeddings = OpenAIEmbeddings()
         self.vectorstore = None
         self.rag_chain = None
         self.clear_store = clear_store
-        self.vectorstore_retriever = None
+        self.base_retriever = None
+        self.final_retriever = None
         self.bm25_retriever = None
         self.existing_vectorstore = existing_vectorstore
         self.ensemble_retriever = None
         self.use_ensemble_retriever = use_ensemble_retriever
+        self.use_multiquery = use_multiquery
         self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
         self.k_documents = 5
@@ -72,18 +78,16 @@ class RAGSystem:
         )
         return self.vectorstore
 
-    def setup_vectorstore(self, split_docs: List[str] = None):
+    def setup_vectorstore(self):
         # Initialize the vectorstore - this could be an existing collection
         self.initialize_vectorstore()
         
         if self.clear_store:
             self.vectorstore.drop_tables()
-        
-        if split_docs:
+            # Reinitialize the vectorstore once the tables have been dropped
             self.initialize_vectorstore()
-            self.vectorstore.add_documents(split_docs)
-
-            # caclculate the embedding cost if using openai embeddings
+        
+            # TODO - caclculate the embedding cost if using openai embeddings
             # check instance of embeddings if OpenAIEmbeddings
             # if isinstance(self.embeddings, OpenAIEmbeddings):
                 # calculate cost here
@@ -96,15 +100,23 @@ class RAGSystem:
         self.bm25_retriever = BM25Retriever.from_documents(split_docs)
         self.bm25_retriever.k = self.k_documents
         
-    def setup_basic_retriever(self):
-        self.vectorstore_retriever = self.vectorstore.as_retriever(search_kwargs={"k": self.k_documents})
+    def setup_base_retriever(self):
+        self.base_retriever = self.vectorstore.as_retriever(search_kwargs={"k": self.k_documents})
+        self.final_retriever = self.base_retriever
 
     def setup_ensemble_retriever(self):
-        chroma_retriever = self.vectorstore.as_retriever(search_kwargs={"k": self.k_documents})
+        base_retriever = self.vectorstore.as_retriever(search_kwargs={"k": self.k_documents})
         self.ensemble_retriever = EnsembleRetriever(
-            retrievers=[self.bm25_retriever, chroma_retriever],
+            retrievers=[self.bm25_retriever, base_retriever],
             weights=[0.5, 0.5]
         )
+        self.final_retriever = self.ensemble_retriever
+        
+    def setup_multiquery_retriever(self, retriever):
+        self.final_retriever = MultiQueryRetriever.from_llm(
+                                    retriever=retriever,
+                                    llm=self.llm_queries_generator,
+                            )
 
     def setup_llm(self):
         llm = ChatOpenAI(model_name=self.model_name, temperature=0)
@@ -113,10 +125,7 @@ class RAGSystem:
 
     def setup_rag_chain(self):
         llm = self.setup_llm()
-        if self.use_ensemble_retriever:
-            self.rag_chain = rag_chain_setup(self.ensemble_retriever, llm)
-        else:
-            self.rag_chain = rag_chain_setup(self.vectorstore_retriever, llm)
+        self.rag_chain = rag_chain_setup(self.final_retriever, llm)
 
     def query(self, question: str) -> str:
         result = self.rag_chain.invoke(question)
@@ -124,19 +133,22 @@ class RAGSystem:
 
     def initialize(self):
         self.load_documents()
+        self.setup_vectorstore()
+        self.setup_base_retriever()
         
-        if self.existing_vectorstore:
-            self.setup_vectorstore()
-            self.setup_basic_retriever()
-        else:
-            split_docs = self.prepare_documents()
-            self.setup_vectorstore(split_docs)
+        if not self.existing_vectorstore:
+            # Setup a new vectorstore
+            self.split_docs = self.prepare_documents()
+            
+            self.vectorstore.add_documents(self.split_docs)
 
             if self.use_ensemble_retriever:
-                self.setup_bm25_retriever(split_docs)
+                self.setup_bm25_retriever(self.split_docs)
                 self.setup_ensemble_retriever()
+            elif self.use_multiquery:
+                self.setup_multiquery_retriever(self.base_retriever)
             else:
-                self.setup_basic_retriever()
+                self.setup_base_retriever()
             
         self.setup_rag_chain()
 
