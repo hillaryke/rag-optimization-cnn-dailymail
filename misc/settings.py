e2852683c112ef19230348e7b10b98f4d43ff206
Hillary Kipkemoi <tenskap@gmail.com>
2024-08-01 18:49:35 +0300

    add FastAPI route to use the RAG pipeline

--- misc/settings.py
+++ misc/settings.py
@@ -1,14 +1,15 @@
 import os
 from typing import Final
 
+
 class Settings:
     PG_CONNECTION_STRING: Final = os.getenv("PG_CONNECTION_STRING")
     COLLECTION_NAME: Final = "cnn_dailymail_validation_subset"
-    SOURCE_FILE_PATH: Final = "data/cnn_dailymail_validation_subset.csv"
+    SOURCE_FILE_PATH: Final = "data/collection/cnn_dailymail_validation_subset.csv"
     CHUNK_SIZE: Final = 1000
     CHUNK_OVERLAP: Final = 200
     PAGE_CONTENT_COLUMN: Final = "article"
-    
+
     GENERATOR_TEMPLATE: Final = """
       Use the following pieces of context to answer the question at the end.
       If you don't know the answer, just say that you don't know, don't try to make up an answer.
@@ -17,9 +18,9 @@ class Settings:
       Question: {question}
       Helpful Answer:
     """
-    
+
     # GENERATOR_TEMPLATE: Final = """
-    #     Use the following pieces of context to answer the question at the end. 
+    #     Use the following pieces of context to answer the question at the end.
     #     These are the instruction to consider:
     #     - Prioritize accuracy and conciseness in your response.
     #     - Answer directly and avoid repeating information from the question.
@@ -32,9 +33,9 @@ class Settings:
 
     #       Question: Where is the Eiffel Tower located?
     #       Answer: Paris, France
-        
+
     #     REMEMBER TO FOLLOW THE INSTRUCTIONS ABOVE.
-        
+
     #     Context: {context}
     #     Question: {question}
     #     Answer:
@@ -45,4 +46,3 @@ class Settings:
     EVALUATION_DATASET_DESCRIPTION = """
       Evaluation dataset questions and ground truth answers for RAGAS   pipeline on cnn_dailymail dataset.
     """
-    
