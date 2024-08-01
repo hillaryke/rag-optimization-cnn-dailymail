627af4ad2aef08b0be6813af90b2723a296d634d
Hillary Kipkemoi <tenskap@gmail.com>
2024-08-01 08:09:56 +0300

    add dependencies for usng gemini llm

--- src/rag_pipeline/rag_system.py
+++ src/rag_pipeline/rag_system.py
@@ -30,7 +30,7 @@ CHUNK_OVERLAP = Settings.CHUNK_OVERLAP
 
 class RAGSystem:
     def __init__(self, 
-                model_name: str,
+                model_name: str = None,
                 llm: Any = None,
                 embeddings: Any = None,
                 collection_name: str = COLLECTION_NAME,
@@ -48,11 +48,11 @@ class RAGSystem:
     ):
         self.model_name = model_name
         self.llm = llm
-        self.llm_queries_generator = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+        # self.llm_queries_generator = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
         self.source_file_path = source_file_path
         self.documents = []
         self.split_docs = []
-        self.embeddings = OpenAIEmbeddings()
+        self.embeddings = embeddings if embeddings else OpenAIEmbeddings()
         self.vectorstore = None
         self.rag_chain = None
         self.clear_store = clear_store
